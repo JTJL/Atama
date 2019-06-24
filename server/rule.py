d7eb8231ce5c1a3b883bb87d8c4a645d90df3845
heyshb <heyshb123@gmail.com>
2019-06-24 20:37:52 +0800

    init checkers

--- server/rule.py
+++ server/rule.py
@@ -4,9 +4,9 @@ int num: [1-9] for m,s,p  [1-7] for z
 int index [0-3]
 '''
 
-class card:
+class Card:
   def __init__(self, _color, _num = -1, _index = -1):
-    if (num == -1):
+    if (_num == -1):
       V = _color
       _color = V // (9 * 4)
       _num = (V % (9 * 4)) // 4 + 1
@@ -17,20 +17,21 @@ class card:
     #card.value = (_color, _num, _index)
 
   def __str__(self):
-    if (color == 3):
-      return ["ton","nan","shi","pei","haku","hatsu","chou"][num - 1]
-    ret = str(num) + "mspz"[color]
-    if (num == 5 and index == 0):
+    if (self.color == 3):
+      return ["ton","nan","shi","pei","haku","hatsu","chou"][self.num - 1]
+    ret = str(self.num) + "mspz"[self.color]
+    if (self.num == 5 and self.index == 0):
       ret += '_red'
     return ret
 
   def __int__(self):
     return color * 36 + (num - 1) * 4 + index
 
-class fuuro:
+class Fuuro:
   def __init__(self,_cards):
     self.cards = cards
     
-class handcards:
-  def __init__(self, _cards, _fuuros):
-    
+class Cards:
+  def __init__(self, _handcards, _fuuros):
+    self.handcards = _handcards
+    self.fuuros = _fuuros
